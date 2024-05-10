72eb698d535d71158e03d362181b4a92e3e539fc
Oscar996 <72895074+Oscar996@users.noreply.github.com>
2024-05-10 10:30:06 -0400

    Update docker-zabbix-grafana-latest.sh

--- docker-zabbix-grafana-latest.sh
+++ docker-zabbix-grafana-latest.sh
@@ -18,7 +18,7 @@ docker pull grafana/grafana
 docker run --name mysql-server -t -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="pzabbix" -e MYSQL_ROOT_PASSWORD="pzabbix" -d mysql --character-set-server=utf8 --collation-server=utf8_bin --default-authentication-plugin=mysql_native_password
 docker run --name zabbix-java-gateway -t --restart unless-stopped -d zabbix/zabbix-java-gateway
 docker run --name zabbix-server-mysql -t -e DB_SERVER_HOST="mysql-server" -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="pzabbix" -e MYSQL_ROOT_PASSWORD="pzabbix" -e ZBX_JAVAGATEWAY="zabbix-java-gateway" --link mysql-server:mysql --link zabbix-java-gateway:zabbix-java-gateway -p 10051:10051 --restart unless-stopped -d zabbix/zabbix-server-mysql:alpine-6.0-latest
-docker run --name zabbix-web-apache-mysql -t -e DB_SERVER_HOST="mysql-server" -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="pzabbix" -e MYSQL_ROOT_PASSWORD="pzabbix" --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -p 80:8080 --restart unless-stopped -d zabbix/zabbix-web-apache-mysql
+docker run --name zabbix-web-apache-mysql -t -e DB_SERVER_HOST="mysql-server" -e ZBX_SERVER_HOST="zabbix-server" -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="pzabbix" -e MYSQL_ROOT_PASSWORD="pzabbix" --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -p 80:8080 --restart unless-stopped -d zabbix/zabbix-web-apache-mysql
 docker run --name zabbix-agent --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -e ZBX_HOSTNAME="Zabbix server" -e ZBX_SERVER_HOST="zabbix-server" -d zabbix/zabbix-agent
 docker run --name grafana -d -p 3000:3000  -e "GF_INSTALL_PLUGINS=alexanderzobnin-zabbix-app" grafana/grafana
 
